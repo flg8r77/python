7623d1875b35e13102860e957c3b78c22716f74e
flg8r77 <flg8r77@gmail.com>
2023-08-14 11:36:06 -0500

    :wrench: bug fixes

--- ex91.py
+++ ex91.py
@@ -21,32 +21,7 @@ into another program.
 
 def main():
 
-    while True:
-        try:
-            year = int(input("Enter the year: "))
-        except ValueError:
-            continue
-        else:
-            break
-    while True:
-        try:
-            month = int(input("Enter the month (number): "))
-        except ValueError:
-            continue
-        else:
-            break
-    while True:
-        try:
-            day = int(input("Enter the day (number): "))
-        except ValueError:
-            continue
-        else:
-            break
-    od = ordinaldate(day, month, year)
-    print(f'{od} day of the year')
-
-def ordinaldate(day, month, year):
-    months = {
+    days_in_month = {
         '1': 31,
         '2': 28,
         '3': 31,
@@ -60,22 +35,80 @@ def ordinaldate(day, month, year):
         '11': 30,
         '12': 31
     }
-    days = 0
-    for i in range(1, month):
-        days = days + months[str(month)]
-    if month > 2:    
-        if year % 4 == 0:
+
+    while True:
+        try:
+            year = int(input("Enter the year: "))
+        except ValueError:
+            continue
+        else:
+            break
+    
+    leap_year = leapyear(year)
+    
+    while True:
+        try:
+            month = int(input("Enter the month (number 1-12): "))
+            if month > 12:
+                raise ValueError
+        except ValueError:
+            continue
+        else:
+            break
+    
+    while True:
+        try:
+            day = int(input("Enter the day (number): "))
+            if not leap_year and month == 2 and day > 28:
+                raise ValueError
+            if month != 2 and day > days_in_month[str(month)]:
+                raise ValueError
+        except ValueError:
+            continue
+        else:
+            break
+    
+    od = ordinaldate(day, month, year)
+    print(f'{od} day of the year')
+
+def leapyear(year):
+    if year % 4 == 0:
             if year % 100 == 0:
                 if year % 400 == 0:
-                    return 1 + days + day
+                    return True
                 else:
-                    return days + day
+                    return False
             else:
-                return 1 + days + day
-        else:
-            return days + day
+                return True
+    else:
+        return False
+
+def ordinaldate(day, month, year):
+    
+    days_dict = {
+        '1': 31,
+        '2': 28,
+        '3': 31,
+        '4': 30,
+        '5': 31,
+        '6': 30,
+        '7': 31,
+        '8': 31,
+        '9': 30,
+        '10': 31,
+        '11': 30,
+        '12': 31
+    }
+
+    days = 0
+    for i in range(1, month):
+        days = days + days_dict[str(i)]
+    
+    if month > 2 and leapyear(year):
+        return days + day + 1
     else:
         return days + day
+    
 
 if __name__ == "__main__":
     main()
