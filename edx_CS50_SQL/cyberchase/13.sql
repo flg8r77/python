dd959191dc62b2f739a5a58da34775576e35484f
flg8r77 <flg8r77@gmail.com>
2023-10-24 08:28:54 -0500

    :wrench: CS50 SQL exercise

---
+++ edx_CS50_SQL/cyberchase/13.sql
@@ -0,0 +1,1 @@
+SELECT "title" from "episodes" WHERE ("season" >= 1 AND "season" <= 7) AND "topic" LIKE '%logic%';
---
+++ edx_CS50_SQL/cyberchase/6.sql
@@ -0,0 +1,1 @@
+SELECT "title" from "episodes" WHERE "season" = 6 AND "air_date" <= "2007-12-31";
